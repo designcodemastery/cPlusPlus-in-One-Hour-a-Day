b8c6a4d9ba5e406ea514d8c80d86c9e273df5945
designcodemastery <designcodemastery@proton.me>
2024-08-27 09:27:36 -0700

    Lesson 2 Examples

---
+++ Lesson-2/example2-2.cpp
@@ -0,0 +1,18 @@
+// Example using namespace Declaration
+
+// Preprocessor directive
+#include <iostream>
+
+// Start of the program
+int main() {
+
+    // Tell the compiler what namespace to search in
+    using namespace std;
+
+    /* Write to the screen using std::cout */
+    cout << "Hello Martin, Welcome to the world of C++!" << endl;
+
+    // Return a value to the OS
+    return 0;
+
+}
---
+++ Lesson-2/example2-4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// Declare a function
+int DemoConsoleOutput();
+
+string AdditionalConsoleOutput();
+
+// Start of the program
+int main() {
+
+    // Call i.e. invoke the function
+    DemoConsoleOutput();
+
+    return 0;
+
+}
+
+// Define i.e. implement the previously declared functions
+
+string AdditionalConsoleOutput() {
+
+    return "More text from my AdditionConsoleOutput funnction";
+
+}
+int DemoConsoleOutput() {
+
+    cout << "This is a simple string literal" << endl;
+    cout << "Writing number five: " << 5 << endl;
+    cout << "Performing division 10 / 5 = " << 10 / 5 << endl;
+    cout << "Pi when approximated is 22 / 7 = " << 22 / 7 << endl;
+    cout << "Pi is 22 / 7 = " << 22.0 / 7 << endl;
+
+    cout << "Let's see if my function can be part of the output stream: " << AdditionalConsoleOutput() << "?" << endl;
+
+    return 0;
+}
